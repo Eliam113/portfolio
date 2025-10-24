25f46dc06f6fc91a2cba4ae0711ea04b7f248e40
ELIAM MPUTU <51476594+Eliam113@users.noreply.github.com>
2025-10-24 10:47:58 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export', // required for static export
+  basePath: isProd ? '/portfolio' : '', // GitHub Pages subpath
+  images: { unoptimized: true }, // disables image optimization (not supported on GH Pages)
+};
+
+module.exports = nextConfig;
